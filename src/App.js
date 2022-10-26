3b405ca62e81edc4e5f5c61e300d621267c4391c
namnh16 <hnam9430@gmail.com>
2022-10-26 21:53:09 +0700

    Dùng template khác
    - Do template cũ có jQuery không tích hợp với React được
    - WIP: landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header";
+import About from "components/About";
+import Contact from "components/Contact";
+import Footer from "components/Footer";
+import Header from "components/Header"; //Include Header
+import Home from "components/Home";
+import Products from "components/Products";
+import SingleProduct from "components/SingleProduct";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/single-product" element={<SingleProduct />} />
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,171 +1,86 @@
-const Header = () => {
+import { Link } from "react-router-dom";
+
+function Header() {
   return (
-    <>
-      <header class="header_area sticky-header">
-        <div class="main_menu">
-          <nav class="navbar navbar-expand-lg navbar-light main_box">
-            <div class="container">
-              <a class="navbar-brand logo_h" href="index.html">
-                <img src="img/logo-no-background-150x70.png" alt="" />
+    <header className="header-area header-sticky">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="main-nav">
+              <Link className="logo" to={{ pathname: "/" }}>
+                blackadam
+              </Link>
+
+              <ul className="nav">
+                <li className="scroll-to-section">
+                  <a href="#top" className="active">
+                    Home
+                  </a>
+                </li>
+                <li className="scroll-to-section">
+                  <a href="#men">Men's</a>
+                </li>
+                <li className="scroll-to-section">
+                  <a href="#women">Women's</a>
+                </li>
+                <li className="scroll-to-section">
+                  <a href="#kids">Kid's</a>
+                </li>
+                <li className="submenu">
+                  <a href="javascript:;">Pages</a>
+                  <ul>
+                    <li>
+                      <Link to={{ pathname: "/about" }}>About Us</Link>
+                    </li>
+                    <li>
+                      <Link to={{ pathname: "/products" }}>Products</Link>
+                    </li>
+                    <li>
+                      <Link to={{ pathname: "/single-product" }}>
+                        Single Product
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to={{ pathname: "/contact-us" }}>Contact Us</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="submenu">
+                  <a href="javascript:;">Features</a>
+                  <ul>
+                    <li>
+                      <a href="#">Features Page 1</a>
+                    </li>
+                    <li>
+                      <a href="#">Features Page 2</a>
+                    </li>
+                    <li>
+                      <a href="#">Features Page 3</a>
+                    </li>
+                    <li>
+                      <a
+                        rel="nofollow"
+                        href="https://therichpost.com/page/4"
+                        target="_blank"
+                      >
+                        Template Page 4
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="scroll-to-section">
+                  <a href="#explore">Explore</a>
+                </li>
+              </ul>
+              <a className="menu-trigger">
+                <span>Menu</span>
               </a>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <div
-                class="collapse navbar-collapse offset"
-                id="navbarSupportedContent"
-              >
-                <ul class="nav navbar-nav menu_nav ml-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="index.html">
-                      Home
-                    </a>
-                  </li>
-                  <li class="nav-item submenu dropdown">
-                    <a
-                      href="#"
-                      class="nav-link dropdown-toggle"
-                      data-toggle="dropdown"
-                      role="button"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      Shop
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li class="nav-item">
-                        <a class="nav-link" href="category.html">
-                          Shop Category
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="single-product.html">
-                          Product Details
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="checkout.html">
-                          Product Checkout
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="cart.html">
-                          Shopping Cart
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="confirmation.html">
-                          Confirmation
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item submenu dropdown">
-                    <a
-                      href="#"
-                      class="nav-link dropdown-toggle"
-                      data-toggle="dropdown"
-                      role="button"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      Blog
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li class="nav-item">
-                        <a class="nav-link" href="blog.html">
-                          Blog
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="single-blog.html">
-                          Blog Details
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item submenu dropdown">
-                    <a
-                      href="#"
-                      class="nav-link dropdown-toggle"
-                      data-toggle="dropdown"
-                      role="button"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      Pages
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li class="nav-item">
-                        <a class="nav-link" href="login.html">
-                          Login
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="tracking.html">
-                          Tracking
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="elements.html">
-                          Elements
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contact.html">
-                      Contact
-                    </a>
-                  </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <li class="nav-item">
-                    <a href="#" class="cart">
-                      <span class="ti-bag"></span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <button class="search">
-                      <span class="lnr lnr-magnifier" id="search"></span>
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-        <div class="search_input" id="search_input_box">
-          <div class="container">
-            <form class="d-flex justify-content-between">
-              <input
-                type="text"
-                class="form-control"
-                id="search_input"
-                placeholder="Search Here"
-              />
-              <button type="submit" class="btn"></button>
-              <span
-                class="lnr lnr-cross"
-                id="close_search"
-                title="Close Search"
-              ></span>
-            </form>
+            </nav>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
-};
+}
 
 export default Header;
